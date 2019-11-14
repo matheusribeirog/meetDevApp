9fba06632804d3ce5b685ef55f1a4e215f82e627
Matheus Ribeiro <30762136+matheusribeirog@users.noreply.github.com>
2019-11-14 10:48:56 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const cors=require('cors')
 
 const server=express()
 
-mongoose.connect('YOUR MONGO DB CONNECTION',{useNewUrlParser:true})
+mongoose.connect('HTTPS://YOUR MONGO DB CONNECTION',{useNewUrlParser:true})
 
 server.use(cors());
 
